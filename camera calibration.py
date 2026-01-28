0a0353da57caf5a046fa12f145a880de744cfaed
daxigua2123 <17863123831@163.com>
2026-01-28 16:36:46 +0800

    Update camera calibration.py

--- camera calibration.py
+++ camera calibration.py
@@ -1,1 +1,15 @@
-
+# -*- coding: utf-8 -*-
+from calibration import Calibrator as Calibrator
+import numpy as np
+def main():
+    img_dir = "......."   
+    calibrator = Calibrator(img_dir)
+    calibrator.visualization()
+    # calibrate the camera
+    mat_intri, coff_dis,r,t = calibrator.calibrate_camera()
+    print(mat_intri)
+    print(coff_dis)
+    # calibrator.visualization()
+    np.savez('camera_para_260113_3.npz', intri=mat_intri, dis=coff_dis)
+if __name__ == '__main__':
+    main()
